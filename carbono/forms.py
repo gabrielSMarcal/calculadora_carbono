43687c4f8d385f1b490a96b1313ee2308d6bb100
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-10-10 21:53:25 -0300

    Temporary do Forms

---
+++ carbono/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+'''
+class CarroForms(forms.Form):
+    base_do_calculo=forms.FloatField(
+        label='Km rodado no mês'
+        required=True,
+        max_value=100000,
+        blank=False,
+    )
+
+class EnergiaForms(forms.Form):
+    base_do_calculo=forms.FloatField(
+        label='Kilowatts gasto no mês'
+        required=True,
+        max_value=1000000,
+        blank=False,
+    )
+    
+class ContaForms(forms.Form):
+    base_do_calculo=forms.FloatField(
+        label='Valor da sua conta de Luz'
+        required=True,
+        max_value=1000,
+        blank=False,
+    )
+    
+'''
