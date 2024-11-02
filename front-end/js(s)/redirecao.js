478cb1a66e448bc4f572fe31476192d41e5a3565
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-11-02 11:17:13 -0300

    Ajuste de diretórios

--- front-end/js(s)/redirecao.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.funcionam').forEach((botao) => {
-  botao.addEventListener('click', () => {
-    window.location.href = 'calculo_carbono.html'; // Redireciona para a página de cálculo
-  });
-});
