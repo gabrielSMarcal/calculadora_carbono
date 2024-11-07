00ae1c646e117660a23fc675743e66775ddafef6
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-11-07 08:36:27 -0300

    Complemento de comentários

--- carbono/views.py
+++ carbono/views.py
@@ -3,115 +3,45 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from math import ceil
 import requests
-import os
+
 
 from .models import Carro, Energia, Gas
 
-
 '''
-************************************** CONSTANTES **************************************
+**************
+* CONSTANTES *
+**************
 '''
 MEDIA_CC = 63.87
 CUSTO_POR_ARVORE = 35
 CONV_ENERGIA = 0.37
 
 # URL da API e chave de acesso
-API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
-API_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/EUR"
+API_URL = f"https://v6.exchangerate-api.com/v6/b4da300231791b77b91124b8/latest/EUR" #Key gratuita, disponibilizada para finalidade de ser executada por qualquer avaliador
 
+# Função para obter a taxa de câmbio do euro e converter em reais
 def get_euro_exchange_rate():
     try:
         response = requests.get(API_URL)
         response.raise_for_status()
         data = response.json()
-        return data['conversion_rates']['BRL']  # Supondo que você quer a taxa de EUR para BRL
+        return data['conversion_rates']['BRL']  
     except requests.RequestException as e:
         print(f"Erro ao obter a taxa de câmbio: {e}")
         return None
 
 # Atualizando a constante com o valor atual do euro
-MEDIA_EURO = get_euro_exchange_rate() or 6.5
+MEDIA_EURO = get_euro_exchange_rate()
 
+'''
+****************************
+* RENDERIZAÇÃO DAS PÁGINAS *
+****************************
+'''
 
 # Renderização da página inicial
 def index(request):
     return render(request, 'carbono/index.html')
-
-'''
-************************************** FUNÇÕES DAS CALCULADORAS **************************************
-'''
-
-# Função para cálculo do valor do CC futuro baseado na TCO2
-def valor_da_tonelada(co):
-    media_valor_credito = (MEDIA_CC * MEDIA_EURO)  # Média dos últimos 12 meses
-    
-    valor = co * media_valor_credito
-    return valor
-
-# Função para cálculo de índice de árvores necessárias para compensar, baseado na TCO2
-def arvores(co):
-    ABSORCAO = 0.37  # Cálculo baseado do site IDESAM
-    
-    arvore = co / ABSORCAO
-    custo = ceil(arvore) * CUSTO_POR_ARVORE 
-    return (arvore, custo)
-
-'''
-******************* FUNÇÕES PARA CÁLCULO DE CARBONO, RETORNANDO SEMPRE O VALOR MENSAL (credito) E ANUAL (anual) *******************
-'''
-
-# Função para calcular carbono de carro, baseado na quilometragem por mês.
-def carro(km_por_mes, carro_obj):
-    km_por_mes = float(km_por_mes)
-    emissao = (km_por_mes / carro_obj.consumo) * carro_obj.emissao
-    credito = emissao / 1000
-    anual = credito * 12
-    
-    return (credito, anual)
-
-# Função para calcular carbono de energia, baseada na quantidade de Kilowatts por hora no mês.
-def energia_kwh(kwh_usado, energia_obj):
-    kwh_usado = float(kwh_usado)    
-    emissao = kwh_usado * energia_obj.emissao
-    credito = emissao / 1000
-    anual = credito * 12
-    
-    return (credito, anual)
-
-# Função para calcular carbono de energia, baseada no valor monetário da conta de luz do usuário.
-def energia_reais(valor_da_conta, energia_obj):
-    valor_da_conta = float(valor_da_conta)
-    conversao = valor_da_conta / CONV_ENERGIA
-    emissao = conversao * energia_obj.emissao
-    credito = emissao / 1000
-    anual = credito * 12
-    
-    return (credito, anual)
-
-# Função para calcular carbono de ônibus, baseado na quilometragem mensal usada pelo usuário, dividido por uma média de passageiros.
-def onibus(km_por_mes_onibus, carro_obj):
-    km_por_mes_onibus = float(km_por_mes_onibus)
-    emissao = (km_por_mes_onibus / carro_obj.consumo) * carro_obj.emissao / 40  # Média de 40 passageiros por ônibus
-    credito = emissao / 1000
-    anual = credito * 12
-
-    return (credito, anual)
-
-# Função para calcular carbono de gás de cozinha, baseado no numero de botijões (13kg) usados por mês.
-def gas_botijao(botijao, gas_obj):
-    botijao = float(botijao) 
-    credito = (botijao * gas_obj.emissao) / 1000
-    anual = credito * 12
-
-    return (credito, anual)
-
-# Função para calcular barbono de gás de cozinha, baseado no valor em m³ de gás encanado usado por mês.
-def gas_encanado(area, gas_obj):
-    area = float(area) 
-    credito = (area * gas_obj.emissao) / 1000
-    anual = credito * 12
-
-    return (credito, anual)
 
 # Renderização da página da calculadora, absorvendo os valores inseridos no site para retornar os resultados.
 def calculadora(request):
@@ -278,10 +208,95 @@ def calculadora(request):
         'onibus': onibus,
         'gases': gases,
     })
+    
+
+'''
+****************************
+* FUNÇÕES DAS CALCULADORAS *
+****************************
+'''
+
+# Função para cálculo do valor do CC futuro baseado na TCO2
+def valor_da_tonelada(co):
+    media_valor_credito = (MEDIA_CC * MEDIA_EURO)  # Média dos últimos 12 meses
+    
+    valor = co * media_valor_credito
+    return valor
+
+# Função para cálculo de índice de árvores necessárias para compensar, baseado na TCO2
+def arvores(co):
+    ABSORCAO = 0.37  # Cálculo baseado do site IDESAM
+    
+    arvore = co / ABSORCAO
+    custo = ceil(arvore) * CUSTO_POR_ARVORE 
+    return (arvore, custo)
+
+'''
+***********************************************************************************************
+* FUNÇÕES PARA CÁLCULO DE CARBONO, RETORNANDO SEMPRE O VALOR MENSAL (credito) E ANUAL (anual) *
+***********************************************************************************************
+'''
+
+# Função para calcular carbono de carro, baseado na quilometragem por mês.
+def carro(km_por_mes, carro_obj):
+    km_por_mes = float(km_por_mes)
+    emissao = (km_por_mes / carro_obj.consumo) * carro_obj.emissao
+    credito = emissao / 1000
+    anual = credito * 12
+    
+    return (credito, anual)
+
+# Função para calcular carbono de energia, baseada na quantidade de Kilowatts por hora no mês.
+def energia_kwh(kwh_usado, energia_obj):
+    kwh_usado = float(kwh_usado)    
+    emissao = kwh_usado * energia_obj.emissao
+    credito = emissao / 1000
+    anual = credito * 12
+    
+    return (credito, anual)
+
+# Função para calcular carbono de energia, baseada no valor monetário da conta de luz do usuário.
+def energia_reais(valor_da_conta, energia_obj):
+    valor_da_conta = float(valor_da_conta)
+    conversao = valor_da_conta / CONV_ENERGIA
+    emissao = conversao * energia_obj.emissao
+    credito = emissao / 1000
+    anual = credito * 12
+    
+    return (credito, anual)
+
+# Função para calcular carbono de ônibus, baseado na quilometragem mensal usada pelo usuário, dividido por uma média de passageiros.
+def onibus(km_por_mes_onibus, carro_obj):
+    km_por_mes_onibus = float(km_por_mes_onibus)
+    emissao = (km_por_mes_onibus / carro_obj.consumo) * carro_obj.emissao / 40  # Média de 40 passageiros por ônibus
+    credito = emissao / 1000
+    anual = credito * 12
+
+    return (credito, anual)
+
+# Função para calcular carbono de gás de cozinha, baseado no numero de botijões (13kg) usados por mês.
+def gas_botijao(botijao, gas_obj):
+    botijao = float(botijao) 
+    credito = (botijao * gas_obj.emissao) / 1000
+    anual = credito * 12
+
+    return (credito, anual)
+
+# Função para calcular barbono de gás de cozinha, baseado no valor em m³ de gás encanado usado por mês.
+def gas_encanado(area, gas_obj):
+    area = float(area) 
+    credito = (area * gas_obj.emissao) / 1000
+    anual = credito * 12
+
+    return (credito, anual)
+
+
 
 
 '''
-******************* FUNÇÕES PARA LIMPAR OS RESULTADOS DOS CÁLCULOS, INDIVIDUALMENTE OU TODOS JUNTOS *******************
+***********************************************************************************
+* FUNÇÕES PARA LIMPAR OS RESULTADOS DOS CÁLCULOS, INDIVIDUALMENTE OU TODOS JUNTOS *
+***********************************************************************************
 '''
 
 def limpar_carro(request):
